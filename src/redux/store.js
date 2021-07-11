dc0fb2ef1623cba546fe27fd27073096566a3ef5
Karolina Michalak <karolina0michalak@gmail.com>
2021-07-11 21:44:10 +0200

    Initializing store using redux toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import muffinsReducer from "./muffins";
+import { configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
   muffins: muffinsReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore({
+  reducer: { muffins: muffinsReducer },
+});
 
 export default store;
